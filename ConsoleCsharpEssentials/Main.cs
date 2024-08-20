aad063517557bf54db66b7efdac9d12ae9a6dcd5
ɐllɐ <molchanova.a.a@gmail.com>
2024-08-20 22:19:10 +0300

    questionnaire

--- ConsoleCsharpEssentials/Main.cs
+++ ConsoleCsharpEssentials/Main.cs
@@ -7,8 +7,15 @@ class MainClass
 
         string myName;
         myName = "Alla";
+        int myAge = 35;
+        bool isHavePet = true;
+        float shoeSize = 38.5f;
 
-        Console.WriteLine(myName);
+        Console.WriteLine("Hi, I am {0}",myName);
+        Console.WriteLine("I am {0} years old", myAge);
+        Console.WriteLine("Do I have a pet? It is {0}",isHavePet);
+        Console.WriteLine("I am wearing shoes of {0} size",shoeSize);
+
         Console.WriteLine("\u0023");
 
         Console.ReadKey();
