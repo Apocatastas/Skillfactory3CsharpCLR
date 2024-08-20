9ef84db49bc779b9faa87b3be33bd57ed56891dc
ɐllɐ <molchanova.a.a@gmail.com>
2024-08-20 22:37:33 +0300

    Semaphore

--- ConsoleCsharpEssentials/Main.cs
+++ ConsoleCsharpEssentials/Main.cs
@@ -1,11 +1,22 @@
 ﻿using System;
 
+
+
 class MainClass
 {
+    enum Semaphore
+    {
+        Red = 100,
+        Yellow = 200,
+        Green = 300
+    }
+
     public static void Main(string[] args)
     {
 
-        string myName;
+
+
+    string myName;
         myName = "Alla";
         int myAge = 35;
         bool isHavePet = true;
@@ -15,7 +26,7 @@ class MainClass
         Console.WriteLine("I am {0} years old", myAge);
         Console.WriteLine("Do I have a pet? It is {0}",isHavePet);
         Console.WriteLine("I am wearing shoes of {0} size",shoeSize);
-
+        Console.WriteLine(Semaphore.Green);
         Console.WriteLine("\u0023");
 
         Console.ReadKey();
